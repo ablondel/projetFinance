e297011aef94baaf9c5bcb0ea1078f5b0770f941
ablondel <ablondelablondel0002@enseirb.fr>
2016-12-03 17:05:02 +0100

    test Persistance en cours....

--- AppBank.java
+++ AppBank.java
@@ -14,16 +14,20 @@ public class AppBank
     InterBank interbank = null;
     try
     {
-      //CLIENT of InterBanK
+      /* CLIENT of InterBanK */
       System.out.println(args[0]);
+      // create and initialize the ORB
       org.omg.CORBA.Object objRef;
       String test[] = new String[2];
       test[0] = "-ORBInitRef";
       test[1] = "NameService=corbaloc::127.0.0.1:2810/NameService";
       ORB orb = ORB.init(test, null);
+       // get the naming service
       objRef = orb.resolve_initial_references("NameService");
       NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
-      objRef = ncRef.resolve_str("app.interbank1");
+      // resolve the object reference from the naming service
+      objRef = ncRef.resolve_str("app.interbank");
+      // convert the CORBA object reference into Echo reference
       interbank = InterBankHelper.narrow(objRef);
     }
     catch (Exception e)
@@ -33,40 +37,64 @@ public class AppBank
     }
     try
     {
-      //Server for Clients
+      /* Server for Clients */
       int numero_bank = Integer.parseInt(args[0]);
       Bank bankA = new Bank(numero_bank, interbank);
       String test[] = new String[2];
       test[0] = "-ORBInitRef";
       test[1] = "NameService=corbaloc::127.0.0.1:2810/NameService";
       ORB orb = ORB.init(test, null);
-      //Push BankTransaction Object to the NameServer
+
+      /** Push BankTransaction Object to the NameServer **/
+      // get reference to rootpoa & activate the POAManager
       org.omg.CORBA.Object objRef = orb.resolve_initial_references("RootPOA");
       POA rootpoa = POAHelper.narrow(objRef);
-      rootpoa.the_POAManager().activate();
+      // Create the Persistent Policy
+      Policy[] persistentPolicy = new Policy[1];
+      persistentPolicy[0] = rootpoa.create_lifespan_policy(LifespanPolicyValue.PERSISTENT);
+      // Create a POA by passing the Persistent Policy
+      POA persistentPOA = rootpoa.create_POA("childPOAT"+numero_bank, null, persistentPolicy );
+      persistentPOA.the_POAManager().activate();
+      // instanciate the servant
+      BankImplTransaction bankImplT = new BankImplTransaction(bankA);
+      persistentPOA.activate_object( bankImplT );
+      // get the naming service
       objRef = orb.resolve_initial_references("NameService");
       NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
-      BankImplTransaction bankImplT = new BankImplTransaction(bankA);
-      objRef = rootpoa.servant_to_reference(bankImplT);
+      // convert the generic CORBA object reference into typed BankTransaction reference
+      objRef = persistentPOA.servant_to_reference(bankImplT);
       BankTransaction bankTransactionRef = BankTransactionHelper.narrow(objRef);
+      // bind the object reference in the naming service
       NameComponent path1[ ] = ncRef.to_name("app.bankTransaction"+numero_bank);
       ncRef.rebind(path1, bankTransactionRef);
       ServerThread st = new ServerThread(orb);
-      st.start();
+      st.start(); // start server
 
-      //Push BankCustomer Object to the NameServer
+      /** Push BankCustomer Object to the NameServer **/
+      // get reference to rootpoa & activate the POAManager
       org.omg.CORBA.Object objRef2 = orb.resolve_initial_references("RootPOA");
       POA rootpoa2 = POAHelper.narrow(objRef2);
-      rootpoa2.the_POAManager().activate();
+
+
+      // Create the Persistent Policy
+      Policy[] persistentPolicy2 = new Policy[1];
+      persistentPolicy2[0] = rootpoa2.create_lifespan_policy(LifespanPolicyValue.PERSISTENT);
+      // Create a POA by passing the Persistent Policy
+      POA persistentPOA2 = rootpoa2.create_POA("childPOAC"+numero_bank, null, persistentPolicy );
+      persistentPOA2.the_POAManager().activate();
+      // instanciate the servant
       BankImplCustomer bankImplC = new BankImplCustomer(bankA);
-      objRef2 = rootpoa2.servant_to_reference(bankImplC);
+      persistentPOA2.activate_object( bankImplC );
+      objRef2 = persistentPOA2.servant_to_reference(bankImplC);
+      // convert the generic CORBA object reference into typed BankCustomer reference
       BankCustomer bankCustomerRef = BankCustomerHelper.narrow(objRef2);
+      // bind the object reference in the naming service
       NameComponent path2[ ] = ncRef.to_name("app.bankCustomer"+numero_bank);
       ncRef.rebind(path2, bankCustomerRef);
       ServerThread st1 = new ServerThread(orb);
-      st1.start();
+      st1.start(); // start server
 
-      //Create Call-Back to InterBank
+      /* Create Call-Back to InterBank */
       bankA.declareMyself();
 
     }
--- AppInterBank.java
+++ AppInterBank.java
@@ -11,25 +11,40 @@ public class AppInterBank
 {
   public static void main(String args[])
   {
+    Properties properties = System.getProperties();
+    properties.put( "org.omg.CORBA.ORBInitialHost", "127.0.0.1" );
+    properties.put( "org.omg.CORBA.ORBInitialPort","2810" );
     try
     {
-      //SERVER for Banks
-      ORB orb = ORB.init(args, null);
+      /* SERVER for Banks */
+      String test[] = new String[2];
+      test[0] = "-ORBInitRef";
+      test[1] = "NameService=corbaloc::127.0.0.1:2810/NameService";
+      ORB orb = ORB.init(test, null);
       org.omg.CORBA.Object objRef = orb.resolve_initial_references("RootPOA");
       POA rootpoa = POAHelper.narrow(objRef);
-      rootpoa.the_POAManager().activate();
+
+      //Create the Persistent Policy
+      Policy[] persistentPolicy = new Policy[1];
+      persistentPolicy[0] = rootpoa.create_lifespan_policy(LifespanPolicyValue.PERSISTENT);
+      //Create a POA by passing the Persistent Policy
+      POA persistentPOA = rootpoa.create_POA("childPOA", null, persistentPolicy );
+      persistentPOA.the_POAManager().activate();
+
+      InterBankImpl interBankImpl = new InterBankImpl();
+      persistentPOA.activate_object( interBankImpl );
+
       objRef = orb.resolve_initial_references("NameService");
       NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
-      InterBankImpl interBankImpl = new InterBankImpl();
-      objRef = rootpoa.servant_to_reference(interBankImpl);
+      objRef = persistentPOA.servant_to_reference(interBankImpl);
       InterBank interBankRef = InterBankHelper.narrow(objRef);
-      NameComponent path1[ ] = ncRef.to_name("app.interbank1");
+      NameComponent path1[ ] = ncRef.to_name("app.interbank");
       ncRef.rebind(path1, interBankRef);
       orb.run();
     }
     catch (Exception e)
     {
-      System.out.println("HelloInterBank Exception: " + e.getMessage());
+      System.err.println("Exception in Persistent Server Startup: " + e.getMessage());
       e.printStackTrace();
     }
   }
